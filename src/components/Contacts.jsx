0c55bca0d387f64767a9f2852dc6077380a9bcd2
Mina Talebi <mina.talebi726@gmail.com>
2025-10-26 14:16:48 +0330

    form first item

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,12 +1,50 @@
+import { useState } from "react";
+
 function Contacts() {
+  const [contact, setContact] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+  const changeHandeler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setContact((contact) => ({ ...contact, [name]: value }));
+  };
+  const addHandeler = () => {};
   return (
     <div>
       <div>
-        <input type="text" name="name" placeholder="Name" />
-        <input type="text" name="lastName" placeholder="Last Name" />
-        <input type="email" name="email" placeholder="Email" />
-        <input type="number" name="phone" placeholder="Phone" />
-        <button> add contact</button>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={contact.name}
+          onChange={changeHandeler}
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          value={contact.lastName}
+          onChange={changeHandeler}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+          onChange={changeHandeler}
+        />
+        <input
+          type="number"
+          name="phone"
+          placeholder="Phone"
+          value={contact.phone}
+          onChange={changeHandeler}
+        />
+        <button onClick={addHandeler}> add contact</button>
       </div>
     </div>
   );
